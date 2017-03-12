dd5aeeee9b15481ae2f29eef94e2a4d2b51582ac
Jason Croft <jasoncroft30@gmail.com>
2017-03-11 23:13:05 -0500

    Quickie "Yahoo! Unsponsored" script

---
+++ unsponsored/yahoo_unsponsored.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Yahoo! Unsponsored
+// @namespace    Misc.
+// @version      0.1
+// @description  Remove "Sponsored" Post on Yahoo! homepage
+// @author       Jason Croft
+// @supportURL   https://github.com/jccrofty30/tampermonkey-scripts/issues
+// @match        https://www.yahoo.com/
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function hideAds() {
+        var ads = document.querySelectorAll("ul.js-stream-tmpl-items > li.js-stream-ad");
+
+        ads.forEach(function(curr) {
+            if (curr.style.display === 'none') {
+                return false;
+            }
+
+            curr.style.display = 'none';
+        });
+    }
+
+    document.querySelector('html').addEventListener('DOMSubtreeModified', function() {
+        hideAds();
+    }, true);
+})();
