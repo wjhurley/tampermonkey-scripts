3707c64cc7c111aeac6104155ff5c3fc4da545a3
Jason Croft <jasoncroft30@gmail.com>
2019-08-09 17:10:30 -0400

    Add hints to pop-over

--- gitlab_tweaks/estimate_adjuster.user.js
+++ gitlab_tweaks/estimate_adjuster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitLab -- Estimate Adjuster
 // @namespace    GLTweaks
-// @version      2019.5.9
+// @version      2019.8.9
 // @description  Add slash-command to edit estimates
 // @author       Jason Croft
 // @supportURL   https://github.com/jccrofty30/tampermonkey-scripts/issues
@@ -19,8 +19,107 @@
         'm': 1,
         'h': 60,
         'd': 480,
-        'w': 2400, // I hope we don't need more 0_o
+        'w': 2400,
     };
+
+    function addHints() {
+        const hintContainer = document.querySelector('#at-view-commands');
+
+        [
+            {
+                action: 'add',
+                command: '/addEstimate',
+                description: 'Add provided arguments to current estimate',
+            },
+            {
+                action: 'subtract',
+                command: '/subtractEstimate',
+                description: 'Subtract provided arguments from current estimate',
+            },
+        ].forEach(action => {
+            const div = document.createElement('div');
+            div.dataset.command = action.command;
+            div.setAttribute(ATTACHED_ATTRIBUTE, '1');
+            div.style.height = '40px';
+
+            const span = document.createElement('span');
+            span.className = 'name';
+            span.innerText = action.command;
+
+            div.appendChild(span);
+
+            const smallParams = document.createElement('small');
+            smallParams.className = 'params';
+            smallParams.innerText = '<1w 3d 2h 14m>';
+
+            div.appendChild(smallParams);
+
+            const smallDescription = document.createElement('small');
+            smallDescription.className = 'description';
+            smallDescription.innerText = action.description;
+
+            div.addEventListener('click', (e) => {
+                const target = e.currentTarget;
+
+                const noteBody = document.querySelector('#note-body');
+                if (noteBody === null) {
+                    return;
+                }
+
+                // https://stackoverflow.com/a/11077016
+                //IE support
+                if (document.selection) {
+                    noteBody.focus();
+                    const sel = document.selection.createRange();
+                    sel.text = target.dataset.command;
+                }
+                //MOZILLA and others
+                else if (noteBody.selectionStart || noteBody.selectionStart === '0') {
+                    const startPos = noteBody.selectionStart;
+                    const endPos = noteBody.selectionEnd;
+                    noteBody.value = [
+                        noteBody.value.substring(0, startPos - 1),
+                        target.dataset.command,
+                        noteBody.value.substring(endPos, noteBody.value.length),
+                    ].join('');
+                    noteBody.selectionStart = startPos + target.dataset.command.length;
+                    noteBody.selectionEnd = startPos + target.dataset.command.length;
+                } else {
+                    noteBody.value += target.dataset.command;
+                }
+
+                noteBody.focus();
+            });
+
+            div.addEventListener('mouseover', () => {
+                const currentCur = document.querySelector('.cur');
+                if (currentCur === null) {
+                    return;
+                }
+
+                currentCur.classList.remove('cur');
+            });
+
+            div.appendChild(smallDescription);
+            hintContainer.appendChild(div);
+        });
+
+        const hackyCss = `
+            #at-view-commands div[${ATTACHED_ATTRIBUTE}] {
+                padding: 8px 15px;
+            }
+            
+            #at-view-commands div[${ATTACHED_ATTRIBUTE}]:hover {
+                background-color: rgb(29, 31, 32);
+                color: rgb(215, 213, 207);
+                cursor: pointer;
+            }`;
+        const style = document.createElement('style');
+
+        style.appendChild(document.createTextNode(hackyCss));
+
+        document.getElementsByTagName('head')[0].appendChild(style);
+    }
 
     function adjustEstimate(target, isAdd) {
         const adjustment = target.value.match(/\/(?:add|subtract)Estimate ([^\r\n]+)/)[1];
@@ -101,6 +200,11 @@
     function parseInput(event) {
         const target = event.currentTarget;
         const value = target.value;
+
+        const customHints = document.querySelector(`#at-view-commands div[${ATTACHED_ATTRIBUTE}]`);
+        if (customHints === null) {
+            addHints();
+        }
 
         if (!/\/(?:add|subtract)Estimate/.test(value)) {
             return;
